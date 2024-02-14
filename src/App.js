d28a8cbd656b91231cb9360750e019627ea6627d
mukrime.sagiroglu <mukrime.sagiroglu@avkaryazilim.com.tr>
2024-02-14 23:54:09 +0300

    notlar listelenip silindi

--- src/App.js
+++ src/App.js
@@ -9,16 +9,17 @@ import "./App.css";
 import Login from "./components/Login";
 import Home from './components/Home';
 import Dashboard from './components/Dashboard';
-
+import GradeList from './components/GradeList';
 function App() {
 	return (
 		<Router>
 			<div className="App">
 				<Routes>
 					<Route path="/" element={<Navigate replace to="/login" />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/home" element={<Home />} />
-					<Route path="/dashboard" element={<Dashboard />} />
+					<Route path="/login" element={<Login/>} />
+					<Route path="/home" element={<Home/>} />
+					<Route path="/dashboard" element={<Dashboard/>} />
+					<Route path="/gradeList" element={<GradeList/>} />
 				</Routes>
 			</div>
 		</Router>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+	const navigate = useNavigate();
+
 	return (
-		<div
-			style={{ maxWidth: "600px", margin: "50px auto", textAlign: "center" }}
-		>
+		<div style={{ maxWidth: "600px", margin: "50px auto", textAlign: "center" }}>
 			<h1>Hoş Geldiniz</h1>
 			<p>Bu, Öğretmen Bilgi Sistemi'nin Ana Sayfasıdır.</p>
-			{}
+			<button onClick={() => navigate('/gradeList')} style={{ padding: "10px 20px", marginTop: "20px" }}>
+				Notları Listele
+			</button>
 		</div>
 	);
 };
+
 export default Home;
