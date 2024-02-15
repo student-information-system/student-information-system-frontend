8e01dd756d4c463c30a7e0bae5c4ca5e77d1b124
mukrime.sagiroglu <mukrime.sagiroglu@avkaryazilim.com.tr>
2024-02-15 23:55:53 +0300

    Güncelleme düzeltildi

--- src/components/GradeList.js
+++ src/components/GradeList.js
@@ -1,6 +1,7 @@
 import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
 import React, { useEffect, useState } from "react";
 import "./GradeList.css";
+
 function GradesList() {
 	const [grades, setGrades] = useState([]);
 
@@ -17,7 +18,7 @@ function GradesList() {
 			const gradesData = await response.json();
 			const gradesWithNames = gradesData.map((grade) => ({
 				...grade,
-				studentName: `${grade.firstName} ${grade.lastName}`, 
+				studentName: `${grade.firstName} ${grade.lastName}`,
 			}));
 			setGrades(gradesWithNames);
 		} catch (error) {
@@ -45,15 +46,22 @@ function GradesList() {
 	const handleUpdate = async (grade) => {
 		const newGrade = prompt("Yeni notu giriniz:", grade.grade);
 		if (newGrade != null && newGrade !== "") {
+			const gradeData = {
+				gradeId: grade.grade_id, // Bu alanı doğru şekilde kullanıyoruz
+				grade: parseInt(newGrade, 10), // Notu integer'a çeviriyoruz
+				studentId: grade.studentId,
+				courseId: grade.courseId,
+			};
+
 			try {
 				const response = await fetch(
-					`http://localhost:8080/grades/${grade.id}`,
+					`http://localhost:8080/grades/${gradeData.gradeId}`,
 					{
 						method: "PUT",
 						headers: {
 							"Content-Type": "application/json",
 						},
-						body: JSON.stringify({ ...grade, grade: newGrade }),
+						body: JSON.stringify(gradeData),
 					}
 				);
 
@@ -83,10 +91,10 @@ function GradesList() {
 				</thead>
 				<tbody>
 					{grades.map((grade) => (
-						<tr key={grade.id}>
-							<td>
-								{grade.firstName} {grade.lastName}
-							</td>
+						<tr key={grade.gradeId}>
+							{" "}
+							{/* Unique key olarak gradeId kullanıyoruz */}
+							<td>{grade.studentName}</td>
 							<td>{grade.grade}</td>
 							<td>
 								<button
@@ -98,7 +106,7 @@ function GradesList() {
 								</button>
 								<button
 									className="button delete-button"
-									onClick={() => handleDelete(grade.id)}
+									onClick={() => handleDelete(grade.gradeId)}
 								>
 									<DeleteOutlined className="icon" />
 									Sil
